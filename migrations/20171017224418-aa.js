bc1473d2006291b4fe44a12c777f72f7c3060e6c
Saurabh <saurabh.verma2892@gmail.com>
2019-05-21 17:11:57 -0500

    adding new migrations

---
+++ migrations/20171017224418-aa.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    return Promise.all([
+        queryInterface.dropTable('design'),
+        queryInterface.dropTable('product'),
+        queryInterface.dropTable('user'),
+        queryInterface.dropTable('cart'),
+        queryInterface.dropTable('transaction'),
+        queryInterface.dropTable('plan')
+        queryInterface.dropTable('currency')
+        queryInterface.dropTable('discount'),
+        queryInterface.dropTable('cartitem')
+    ])
+     
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+
+  }
+};
